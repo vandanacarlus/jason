91847ac0b9ab82c5cf6d192519ad65e57faf2e31
vandanacarlus <vandanacarlus@gmail.com>
2023-03-18 07:33:28 -0700

    first commit

---
+++ myscript.js
@@ -0,0 +1,19 @@
+Primitive datatype:
+var firstName = "vandana";
+var lastName = "carlus";
+var age = 32
+var name = firstName + " " + lastName
+console.log(name.length);
+console.log(name + age + 1)
+console.log(age + 1 + name)
+var a = 20;
+var b= 2;
+console.log(a+b)
+a = a.toString()
+console.log(a + b)
+console.log(Number(a) + b)
+let url = window.document.url;
+console.log(url)
+
+var ulr = window.document.url
+console.log(url)
---
+++ script.js
@@ -0,0 +1,5 @@
+//Primitive datatype:
+var firstName = "vandana";
+var lastName = "carlus";
+console.log(firstName);
+console.log(lastName);
